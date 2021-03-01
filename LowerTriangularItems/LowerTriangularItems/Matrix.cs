b009d0324539edb7f04602977cf47a095d8e5962
Arsen Dashyan <arsen_dashyan@outlook.com>
2021-03-01 17:22:12 +0400

    Added a LowerTriangular method

---
+++ LowerTriangularItems/LowerTriangularItems/Matrix.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace LowerTriangularItems
+{
+    class Matrix
+    {
+        public static int[][] GetLowTri(int[,] array)
+        {
+            if (array.GetLength(0) == array.GetLength(1))
+            {
+                int[][] result = new int[array.GetLength(1)][];
+
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j <= i; j++)
+                    {
+                        result[i] = new int[j + 1];
+                    }
+                }
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j <= i; j++)
+                    {
+                        result[i][j] = array[i, j];
+                    }
+                }
+                return result;
+            }
+            else
+            {
+                Console.WriteLine("Non correct matrix");
+                return null;
+            }
+            
+        }
+
+        public static void Print(int[][] arr)
+        {
+            foreach (var arrItem in arr)
+            {
+                foreach (var item in arrItem)
+                {
+                    Console.Write(item + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static void Print(int[,] arr)
+        {
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                for (int j = 0; j < arr.GetLength(1); j++)
+                {
+                    Console.Write(arr[i,j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ LowerTriangularItems/LowerTriangularItems/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LowerTriangularItems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            Matrix.Print(array);
+
+            Console.WriteLine(new string('-',20));
+
+            var arr = Matrix.GetLowTri(array);
+            Matrix.Print(arr);
+
+            Console.ReadLine();
+        }
+    }
+}
